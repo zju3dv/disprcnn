0da59e186c0cec5f57c077785c8f5e8de1c486a5
f-sky <939949352@qq.com>
2021-02-02 20:13:35 +0800

    add scripts

--- scripts/car/pob/train_idispnet.sh
+++ scripts/car/pob/train_idispnet.sh
@@ -1,17 +1,16 @@
 echo 'preparing offline predictions...'
-python -m torch.distributed.launch --nproc_per_node $NGPUS tools/test_net.py --config-file configs/kitti/car/pob/mask.yaml
-# generate rois
+python -m torch.distributed.launch --nproc_per_node $NGPUS tools/test_net.py \
+--config-file configs/kitti/car/pob/mask.yaml
+
 echo 'generating rois...'
-#todo
 python tools/kitti_object/generate_psmnet_input_inf.py \
 --output_dir data/car_pob_roi \
---prediction_template models/kitti/pob/mask/inference/kitti_%s_pob/predictions.pth \
---mask_disp_sub_path pob
-# train iDispNet
-#todo
+--shape_prior_base pob
+--prediction_template models/kitti/car/pob/mask/inference/kitti_%s_pob_car/predictions.pth \
+--cls car
+
 echo 'train iDispNet...'
 python -m torch.distributed.launch --nproc_per_node $NGPUS \
 tools/kitti_object/train_idispnet_fa.py \
---data_dir data/car_pob_roi --mode train_oc \
---model_dir models/kitti/pob/idispnet \
---load_model models/PSMNet/pretrained_model_KITTI2015.tar
+--data_dir data/car_pob_roi \
+--model_dir models/kitti/car/pob/idispnet
--- scripts/car/pob/train_rcnn.sh
+++ scripts/car/pob/train_rcnn.sh
@@ -1,10 +1,12 @@
 # copy RPN
-mkdir models/kitti/pob/rcnn #todo
-cp models/kitti/pob/rpn/model_0027462.pth models/kitti/pob/rcnn/model_0000000.pth #todo
+mkdir models/kitti/car/pob/rcnn
+cd models/kitti/car/pob
+cp rpn/model_0027462.pth rcnn/model_0000000.pth
+cd rcnn/
+python -c "import torch;ckpt=torch.load('model_0000000.pth','cpu');ckpt['iteration']=0;torch.save(ckpt,'model_0000000.pth')"
 #todo
-python -c "import torch;ckpt=torch.load('models/kitti/pob/rcnn/model_0000000.pth','cpu');ckpt['iteration']=0;torch.save(ckpt,'models/kitti/pob/rcnn/model_0000000.pth')"
-#todo
-echo models/kitti/pob/rcnn/model_0000000.pth > models/kitti/pob/rcnn/last_checkpoint
+echo $(realpath model_0000000.pth) > last_checkpoint
 # train rcnn
-#todo
-python -m torch.distributed.launch --nproc_per_node $NGPUS tools/train_net.py --config-file configs/kitti/pob/rcnn.yaml
+cd ../../../../../
+python -m torch.distributed.launch --nproc_per_node $NGPUS tools/train_net.py \
+--config-file configs/kitti/car/pob/rcnn.yaml
