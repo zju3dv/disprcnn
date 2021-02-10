12c9d4444de453fcbb64018b0ee29fc2dbdfba56
f-sky <939949352@qq.com>
2021-02-10 09:22:36 +0800

    add scripts to download predictions for cars

---
+++ scripts/download/predictions/car_pob.sh
@@ -0,0 +1,2 @@
+mkdir -p models/kitti/car/pob/rcnn/inference/kitti_val_pob_car
+gdown --id 1am2T_gLaOBjDxBhmcVFl9dw-623uWwtn -O models/kitti/car/pob/rcnn/inference/kitti_val_pob_car/
---
+++ scripts/download/predictions/car_vob.sh
@@ -0,0 +1,2 @@
+mkdir -p models/kitti/car/vob/rcnn/inference/kitti_val_vob_car
+gdown --id 1f_LRpjgQMtIFSwdS56TjtqHFwPXiDp3P -O models/kitti/car/vob/rcnn/inference/kitti_val_vob_car/
